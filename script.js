8dac74de852638a6efcf70f926f52e5f914bb34f
admlidelmed-jpg <admlidelmed@gmail.com>
2026-07-25 10:30:46 -0300

    Create script.js
    criar pasta

---
+++ script.js
@@ -0,0 +1,38 @@
+const auth = firebase.auth();
+let confirmationResult;
+
+const sendCodeBtn = document.getElementById('send-code');
+const verifyCodeBtn = document.getElementById('verify-code');
+const phoneInput = document.getElementById('phone-number');
+const codeInput = document.getElementById('code');
+
+window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container', {
+  'size': 'invisible'
+});
+
+sendCodeBtn.addEventListener('click', () => {
+  const phoneNumber = "+55" + phoneInput.value;
+  auth.signInWithPhoneNumber(phoneNumber, window.recaptchaVerifier)
+    .then((result) => {
+      confirmationResult = result;
+      codeInput.style.display = 'block';
+      verifyCodeBtn.style.display = 'block';
+      alert("Código enviado! Olha o SMS");
+    }).catch((error) => {
+      alert("Erro: " + error.message);
+    });
+});
+
+verifyCodeBtn.addEventListener('click', () => {
+  const code = codeInput.value;
+  confirmationResult.confirm(code).then(() => {
+    document.getElementById('login-box').style.display = 'none';
+    document.getElementById('app-content').style.display = 'block';
+  }).catch(() => {
+    alert("Código inválido");
+  });
+ });
+// CÓDIGO DO BOTÃO BAIXAR APP
+document.getElementById('btnInstalar').addEventListener('click', () => {
+    alert("Para instalar o app:\n1. Clique nos 3 pontinhos do Chrome\n2. Clique em 'Adicionar à tela inicial'\n3. Pronto! Vai aparecer na sua tela");
+});
