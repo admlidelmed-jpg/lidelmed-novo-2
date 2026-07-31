d16b579cec260ea75fa0f96fd828e1c1b941b720
admlidelmed-jpg <admlidelmed@gmail.com>
2026-07-31 14:55:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,22 +4,21 @@ async function carregarProdutos() {
     const produtos = await resposta.json();
     
     const div = document.getElementById('produtos');
-    div.innerHTML = ''; // limpa o "Carregando..."
+    div.innerHTML = '';
 
     produtos.forEach(p => {
       div.innerHTML += `
       <div class="card-produto">
+        <img src="imagens/${p.imagem}" alt="${p.nome}">
         <h3>${p.nome}</h3>
         <p>${p.descricao}</p>
         <p class="preco-avista">À vista: R$ ${p.preco_avista}</p>
-        <p class="preco-parcelado">Parcelado: R$ ${p.preco_parcelado}</p>
+        <p class="preco-parcelado">Parcelado: R$ ${p.precoParcelado || 'Consulte'}</p>
       </div>`;
     });
 
   } catch (erro) {
-    document.getElementById('produtos').innerHTML = 'Erro ao carregar produtos. Recarregue a página.';
-    console.log(erro);
+    document.getElementById('produtos').innerHTML = 'Erro ao carregar produtos.';
   }
 }
-
 carregarProdutos();
