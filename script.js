8b18ee36c76aa043aa6eb2f680f0ae44bb541e6d
admlidelmed-jpg <admlidelmed@gmail.com>
2026-08-11 10:26:22 -0300

    Update script.js
    atualizar

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ const resposta = await fetch('./products.json?v=3007');
         <h3>${p.nome}</h3>
         <p>${p.descricao}</p>
       ${avista > 0? <p class="preco-avista"><b>À vista: R$ ${avista.toFixed(2)}</b></p> : ''}
-      ${cartao > 0? <p class="preco-parcelado">Cartão: R$ ${cartao.toFixed(2)}</p> : ''}
+      ${cartao > 0? <p class="preco-cartao">Cartão: R$ ${cartao.toFixed(2)}</p> : ''}
       <a href="https://wa.me/557398144898?text=Olá! Quero o ${p.nome}" target="_blank" class="btn-whats">Comprar no WhatsApp</a>
       </div>
       `;
