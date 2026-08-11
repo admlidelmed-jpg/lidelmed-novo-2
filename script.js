8d0464b470fbc0914c5c5710467ba3cb684e372d
admlidelmed-jpg <admlidelmed@gmail.com>
2026-08-11 09:14:38 -0300

    Update script.js
    atualizar script

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ async function carregarProdutos() {
         <p>${p.descricao}</p>
       ${avista > 0? <p class="preco-avista"><b>À vista: R$ ${avista.toFixed(2)}</b></p> : ''}
       ${cartao > 0? <p class="preco-parcelado">Cartão: R$ ${cartao.toFixed(2)}</p> : ''}
-        <a href="https://wa.me/557398144898?text=Olá! Quero o ${p.nome}" target="_blank" class="btn-whats">Comprar no Whats</a>
+        <a href="https://wa.me/557398144898?text=Olá! Quero o ${p.nome}" target="_blank" class="btn-whats">Comprar no WhatsApp</a>
       </div>
       `;
     });
